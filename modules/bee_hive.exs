92769eea1695ee0183e947178c01fabce88d5d78
Dmitri Nesterenko <dneseterenko@xogrp.com>
2016-02-08 00:01:53 -0500

    Added use of aliases and module variables
    Using the syntax

--- modules/bee_hive.exs
+++ modules/bee_hive.exs
@@ -2,22 +2,25 @@ defmodule BeeHive do
   import BeeCounter, only: [sum: 1]
   import BeeDivision, only: [gcd: 2]
   import BeeList
-  # do not need the Kernel.length function
+  # Do not need the Kernel.length function
   # we have a BeeList.length
   import Kernel, except: [length: 1]
+  alias BeeDivision, as: B
+  @bees ["Burt", "Bat", "Buzzy", "Bronco"]
 
   @moduledoc ~S"""
-    Your standard issue bee hive full of summed bees,
+    Your standard issue bee hive full of be summation,
     greater common divisors, and bee management
   """
   def buzz do
-    bees = ["Burt", "Bat", "Buzzy", "Bronco"]
-    sum(10)
-    gcd(10,2)
+
+    IO.puts sum(10)
+    IO.puts gcd(10,2)
+    IO.puts B.gcd(12,24)
     # Since there is already Kernel.length this is one way of making it work
-    BeeList.length(bees)
+    BeeList.length(@bees)
     # Not importing Kernel.length is another way
-    length(bees)
+    length(@bees)
   end
 
 end
