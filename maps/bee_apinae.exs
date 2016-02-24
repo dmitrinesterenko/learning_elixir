ac524837d0222e5a06f72a612c441b00b5ebc8b9
Dmitri Nesterenko <dneseterenko@xogrp.com>
2016-02-23 23:12:19 -0500

    Added ways to tell if a bee is flying, if she's a queen or just to call her

--- maps/bee_apinae.exs
+++ maps/bee_apinae.exs
@@ -2,12 +2,20 @@ defmodule BeeApinae do
   @moduledoc """
     This is a bee with a name, some wings and maybe she's a queen
     Usage:
-      %BeeStruct{name: "BeezelBub", wings: false, queen: false}
+      %BeeApinae{name: "BeezelBub", wings: false, queen: false}
   """
   defstruct name: "", wings: true, queen: false
 
-  def flying?(bee = %BeeApinae{}) do
+  def flying?(bee) do
     bee.wings == true && bee.queen == false
+  end
+
+  def call(%BeeApinae{name: name}) when name != "" do
+    IO.puts "Hi there #{name}"
+  end
+
+  def queen?(%BeeApinae{name: name, queen: queen}) when queen == true do
+    IO.puts "#{name} is a Queen!"
   end
 
 
